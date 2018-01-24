f9ad5bd7272e7edeb1d170b52400480388ced305
dragomir <svedava@mail.ru>
2018-01-24 11:47:58 +0000

    decomposed the form by splitting the userInfo to be rendered from Card, then App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Form } from "./form";
+import { Card } from "./card";
 export default class App extends React.Component {
+  state = { data: {} };
+
+  handleChange = data => {
+    this.setState({ data });
+  };
+
   render() {
+    const { data } = this.state;
     return (
       <div>
-        <Form />
+        <Form info={this.handleChange} />
+
+        {data ? <Card data={data} /> : null}
       </div>
     );
   }
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -3,50 +3,31 @@ import { token } from "../../../token";
 import { UserInfo } from "../card/user_info";
 
 export class Form extends React.Component {
-  state = {
-    login: "",
-    data: {}
-  };
-  handleChange = event => {
-    const target = event.target;
-    const value = target.value;
-    console.log(target.name, value);
-    this.setState({ login: value });
-  };
-
   handleSubmit = event => {
     event.preventDefault();
-    const data = JSON.stringify(this.state);
-    console.log("state: ", this.state);
-    fetch(
-      `https://api.github.com/users/${this.state.login}?access_token=${token}`
-    )
+    const value = event.target.username.value;
+    fetch(`https://api.github.com/users/${value}?access_token=${token}`)
       .then(res => {
         if (res.status !== 200) {
           console.log("Something went wrong with the request");
         }
         return res.json();
       })
-      .then(data => this.setState({ data }))
+      .then(data => {
+        this.props.info(data);
+      })
       .catch(err => {
         throw new Error("Fetching failed");
       });
-    this.setState({ login: "" });
   };
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="name">GitHub Username:</label>
-          <input
-            type="text"
-            id="name"
-            value={this.state.input}
-            onChange={this.handleChange}
-          />
+          <input type="text" id="username" />
           <button type="submit">Get my Tamagotchi</button>
         </form>
-        <UserInfo {...this.state.data} />
       </div>
     );
   }
