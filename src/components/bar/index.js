452e38710a72fce6efd62ec06c627c040c3118d1
Natalie <njseeto@users.noreply.github.com>
2018-01-24 13:07:09 +0000

    health bar function added

--- src/components/bar/index.js
+++ src/components/bar/index.js
@@ -1,28 +1,44 @@
 import React from "react";
 import "./style.css";
 export class Bar extends React.Component {
-  move = () => {
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    frame = () => {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
-    };
+  state = {
+    health: 100
+  };
+
+  countdown() {
+    this.setState(prevState => ({
+      health: prevState.health - 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.countdown(), 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  reset = () => {
+    this.setState({
+      health:
+        this.state.health > 90
+          ? this.state.health + 100 - this.state.health
+          : this.state.health + 10
+    });
   };
 
   render() {
-    return (
-      <div>
-        <div id="myProgress">
-          <div id="myBar" />
+    if (this.state.health <= 0) {
+      return <div>No job for you!</div>;
+    } else {
+      return (
+        <div>
+          <div className="Bar">
+            Likelihood to get a job: {this.state.health} %
+          </div>
+          <button onClick={this.reset}>Add experience</button>
         </div>
-        <button onClick={this.move}>Click Me</button>
-      </div>
-    );
+      );
+    }
   }
 }
